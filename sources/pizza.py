1e5b3934c0b6cb021f91775f64db47bf9c81d5ad
Christopher Devaux <christopherdevaux2@gmail.com>
2021-11-19 21:21:31 +0100

    Add new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'My pizza': {
+        'ingredients': { 'mozzarella': 2, 'pepper': 1, 'ham': 2 },
+        'price':9,
     }
 }
